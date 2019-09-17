bd166e55a1ad13959a1695a5fbfc317abc2ad6cf
Lipsita-Senapati <39557902+Lipsita-Senapati@users.noreply.github.com>
2019-09-17 20:53:12 +0530

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,168 @@
+"""Importing of the modules"""
+import fresh_tomatoes
+import media
+
+def main():
+    """Creates 23 Movie objects(Marvel Series), initialising these objects with title, storyline,
+    poster image link, video trailer link and release date."""
+
+    im1 = media.Movie("Iron Man",
+                          "Iron Man is a 2008 American superhero film based on the Marvel Comics character of the same name",
+                          "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
+                          "https://www.youtube.com/watch?v=8hYlB38asDY",
+                          "2008")
+
+    ih = media.Movie("The Incredible Hulk",
+                          "It is the second film in the Marvel Cinematic Universe (MCU)",
+                          "https://upload.wikimedia.org/wikipedia/en/8/88/The_Incredible_Hulk_poster.jpg",
+                          "https://www.youtube.com/watch?v=xbqNb2PFKKA&t=4s",
+                          "2008")
+
+    im2 = media.Movie("Iron Man 2",
+                          "It is the sequel to 2008's Iron Man, and the third film in the Marvel Cinematic Universe (MCU)",
+                          "https://upload.wikimedia.org/wikipedia/en/e/ed/Iron_Man_2_poster.jpg",
+                          "https://www.youtube.com/watch?v=wKtcmiifycU",
+                          "2010")
+
+    ca = media.Movie("Captain America",
+                          "It is the fifth film in the Marvel Cinematic Universe",
+                          "https://upload.wikimedia.org/wikipedia/en/3/37/Captain_America_The_First_Avenger_poster.jpg",
+                          "https://www.youtube.com/watch?v=JerVrbLldXw",
+                          "2011")
+
+
+    th1 = media.Movie("Thor",
+                          "It is the forth film in the Marvel Cinematic Universe (MCU)",
+                          "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg",
+                          "https://www.youtube.com/watch?v=JOddp-nlNvQ",
+                          "2011")
+
+    a1 = media.Movie("The Avengers",
+                          "It is the sixth film in the Marvel Cinematic Universe (MCU)",
+                          "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
+                          "https://www.youtube.com/watch?v=eOrNdBpGMv8",
+                          "2012")
+    
+   
+
+    im3 = media.Movie("Iron Man 3",
+                          "It is the sequel to 2008's Iron Man and 2010's Iron Man 2",
+                          "https://upload.wikimedia.org/wikipedia/en/d/d5/Iron_Man_3_theatrical_poster.jpg",
+                          "https://www.youtube.com/watch?v=Ke1Y3P9D0Bc",
+                          "2013")
+    
+    th2 = media.Movie("Thor: The Dark World",
+                          " It is the sequel to 2011's Thor and the eighth film in the Marvel Cinematic Universe (MCU)",
+                          "https://upload.wikimedia.org/wikipedia/en/7/7e/Thor_-_The_Dark_World_poster.jpg",
+                          "https://www.youtube.com/watch?v=npvJ9FTgZbM",
+                          "2013")
+    
+    cm2 = media.Movie("Captain America: The Winter Soldier",
+                          "It is the sequel to 2011's Captain America",
+                          "https://upload.wikimedia.org/wikipedia/en/e/e8/Captain_America_The_Winter_Soldier.jpg",
+                          "https://www.youtube.com/watch?v=7SlILk2WMTI",
+                          "2014")
+
+    
+    gog1= media.Movie("Guardians of the Galaxy",
+                          "It is the tenth film in the Marvel Cinematic Universe (MCU).",
+                          "https://upload.wikimedia.org/wikipedia/en/b/b5/Guardians_of_the_Galaxy_poster.jpg",
+                          "https://www.youtube.com/watch?v=d96cjJhvlMA",
+                          "2014")
+
+    a2= media.Movie("Avengers: Age of Ultron",
+                          "It is the sequel to 2012's The Avengers and the eleventh film in the Marvel Cinematic Universe (MCU).",
+                          "https://upload.wikimedia.org/wikipedia/en/f/ff/Avengers_Age_of_Ultron_poster.jpg",
+                          "https://www.youtube.com/watch?v=tmeOjFno6Do",
+                          "2014")
+    
+    am= media.Movie("Ant-Man",
+                          " Ant-Man is the name of several fictional superheroes appearing in books published by Marvel Comics..",
+                          "https://upload.wikimedia.org/wikipedia/en/6/6b/Irredeemable_Ant-Man_Vol_1_5_Textless.jpg",
+                          "https://www.youtube.com/watch?v=pWdKf3MneyI",
+                          "2015")
+    
+    cm3 = media.Movie("Captain America:Civil War",
+                          "It is the sequel to 2011's Captain America: The First Avenger",
+                          "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
+                          "https://www.youtube.com/watch?v=dKrVegVI0Us",
+                          "2016")
+
+    ds= media.Movie("Doctor Strange",
+                          "Doctor Strange is a 2016 American superhero film based on the Marvel Comics character of the same name..",
+                          "https://upload.wikimedia.org/wikipedia/en/c/c7/Doctor_Strange_poster.jpg",
+                          "https://www.youtube.com/watch?v=FEWeV2_MAWg",
+                          "2016")
+
+    gog2= media.Movie("Guardians of the Galaxy 2",
+                          "It is the sequel to 2014's Guardians of the Galaxy and the fifteenth film in the Marvel Cinematic Universe (MCU).",
+                          "https://upload.wikimedia.org/wikipedia/en/a/ab/Guardians_of_the_Galaxy_Vol_2_poster.jpg",
+                          "https://www.youtube.com/watch?v=pr7tDrwQ3t8",
+                          "2017")
+    
+    sm1 = media.Movie("Spider-Man: Homecoming",
+                           "Spider-Man: Homecoming is a 2017 American superhero film ",
+                           "https://upload.wikimedia.org/wikipedia/en/f/f9/Spider-Man_Homecoming_poster.jpg",
+                           "https://www.youtube.com/watch?v=n9DwoQ7HWvI",
+                           "J2017")
+
+    th3 = media.Movie("Thor: Ragnarok",
+                          "It is the sequel to 2011's Thor and 2013's Thor: The Dark World",
+                          "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg",
+                          "https://www.youtube.com/watch?v=ue80QwXMRHg",
+                          "2017")
+    
+    a3 = media.Movie("Avengers: Infinity War",
+                          "It is the sequel to 2012's The Avengers and 2015's Avengers: Age of Ultron",
+                          "https://upload.wikimedia.org/wikipedia/en/4/4d/Avengers_Infinity_War_poster.jpg",
+                          "https://www.youtube.com/watch?v=6ZfuNTqbHE8",
+                          "2018")
+    
+   
+
+   
+
+    am2= media.Movie("Ant-Man and the Wasp",
+                          "it is the sequel to 2015's Ant-Man",
+                          "https://upload.wikimedia.org/wikipedia/en/2/2c/Ant-Man_and_the_Wasp_poster.jpg",
+                          "https://www.youtube.com/watch?v=FEWeV2_MAWg",
+                          "2018")
+    
+    
+    bp= media.Movie("Black Panther",
+                          "Black Panther is a 2018 American superhero film ",
+                          "https://upload.wikimedia.org/wikipedia/en/0/0c/Black_Panther_film_poster.jpg",
+                          "https://www.youtube.com/watch?v=xjDjIWPwcPU",
+                          "2018")
+
+    cm1 = media.Movie("Captain Marvel",
+                          "MCU's most powerful superhero",
+                          "https://stat.ameba.jp/user_images/20190108/19/jumbooomori/68/06/j/o0821120014335603368.jpg?caw=800",
+                          "https://www.youtube.com/watch?v=0LHxvxdRnYc",
+                          "March 8, 2019")
+
+    ae = media.Movie("Avengers: EndGame",
+                          "The Avengers take a final stand against Thanos",
+                          "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+                          "https://www.youtube.com/watch?v=TcMBFSGVi1c",
+
+                          "April 26, 2019")
+    
+    
+
+    sm2 = media.Movie("Spider-Man: FFH",
+                           "Following the events of Avengers: Endgame",
+                           "https://terrigen-cdn-dev.marvel.com/content/prod/1x/sffh_venice-high-res.jpg",
+                           "https://www.youtube.com/watch?v=Nt9L1jCKGnE",
+                           "July 5, 2019")
+    
+                    
+    
+    # Store the Movie objects in a list.
+    movies = [im1,ih,im2,th1,ca,a1,im3,th2,cm2,gog1,a2,am,cm3,ds,gog2,sm1,th3,a3,am2,bp,cm1,ae,sm2]
+
+    # Open the movie website in the user's browser, featuring the movies above.
+    fresh_tomatoes.open_movies_page(movies)
+
+if __name__ == '__main__':
+    main()
